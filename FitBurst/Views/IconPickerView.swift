c811e5079daa928b5830193a1fa272f513fd380c
Nikola Sierra <nikola.sierra@outlook.com>
2024-12-26 16:29:23 +0000

    IconPickerView stubbed out!

---
+++ FitBurst/Views/IconPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  IconPickerView.swift
+//  FitBurst
+//
+//  Created by Olga Uskova-Sierra on 26/12/2024.
+//
+
+import SwiftUI
+
+struct IconPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconPickerView()
+}
