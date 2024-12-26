d5484151eb6765850b1f56783f01e6f437e1faec
Nikola Sierra <nikola.sierra@outlook.com>
2024-12-26 16:27:28 +0000

    Added settings page while adding changeable button options for the recording of workouts

--- FitBurst/Views/SettingsView.swift
+++ FitBurst/Views/SettingsView.swift
@@ -28,10 +28,12 @@ enum WorkoutType: Int32 {
 }
 
 struct SettingsView: View {
-    @AppStorage("username") private var userName: String = ""
-    @AppStorage("settingOne") private var settingOne: Bool = true
-    @AppStorage("settingTwo") private var settingTwo: Bool = true
-    @AppStorage("settingThree") private var settingThree: Bool = true
+    @AppStorage("workoutOneOverride") private var workoutOneOverride: String = "Strength"
+    @AppStorage("workoutTwoOverride") private var workoutTwoOverride: String = "Run"
+    @AppStorage("workoutThreeOverride") private var workoutThreeOverride: String = "Team Sport"
+    @AppStorage("workoutFourOverride") private var workoutFourOverride: String = "Cardio"
+    @AppStorage("workoutFiveOverride") private var workoutFiveOverride: String = "Yoga"
+    @AppStorage("workoutSixOverride") private var workoutSixOverride: String = "Martial Arts"
     
     var body: some View {
         ZStack {
@@ -40,32 +42,81 @@ struct SettingsView: View {
             VStack {
                 Text("Settings")
                     .font(.custom("Futura Bold", size: 40))
-                    .foregroundColor(.white)
                     .padding(.bottom, 20)
-
-                Group {
-                    HStack {
-                        Text("Enter your name: ")
-                        Spacer()
-                        TextField("Your name", text: $userName)
+                    .foregroundStyle(.white)
+                
+                VStack (alignment: .leading){
+                    Group {
+                        
+                        Text("Customize your workout types")
+                        Divider().foregroundStyle(Color.white)
+                        
+                        Text("Workout type 1")
+                        TextField("Strength", text: $workoutOneOverride)
                             .frame(width: 150, height:30)
-                            .multilineTextAlignment(.trailing)
-                            .background(.white)
-                        //.overlay(RoundedRectangle(cornerRadius: 2).stroke(.gray))
+                            .background(.white.opacity(0.3))
+                            .cornerRadius(5)
+                        Spacer()
+                        Text("Workout type 2")
+                        TextField("Run", text: $workoutTwoOverride)
+                            .frame(width: 150, height:30)
+                            .background(.white.opacity(0.3))
+                            .cornerRadius(5)
+                        Spacer()
+                        Text("Workout type 3")
+                        TextField("Team Sport", text: $workoutThreeOverride)
+                            .frame(width: 150, height:30)
+                            .background(.white.opacity(0.3))
+                            .cornerRadius(5)
+                        Spacer()
+                        Text("Workout type 4")
+                        TextField("Cardio", text: $workoutFourOverride)
+                            .frame(width: 150, height:30)
+                            .background(.white.opacity(0.3))
+                            .cornerRadius(5)
+                        Spacer()
+                        Text("Workout type 5")
+                        TextField("Yoga", text: $workoutFiveOverride)
+                            .frame(width: 150, height:30)
+                            .background(.white.opacity(0.3))
+                            .cornerRadius(5)
+                        Spacer()
+                        Text("Workout type 6")
+                        TextField("Martial Arts", text: $workoutSixOverride)
+                            .frame(width: 150, height:30)
+                            .background(.white.opacity(0.3))
+                            .cornerRadius(5)
+                        Spacer()
                     }
+                    .foregroundColor(.white)
                     
-                    Toggle("Example setting 1", isOn: $settingOne)
-                    Toggle("Example setting 2", isOn: $settingTwo)
-                    Toggle("Example setting 3", isOn: $settingThree)
+                    Spacer()
                 }
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
-                Spacer()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(35)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 }
 #Preview {
     SettingsView()
 }
+
+
+/* Group {
+ HStack {
+ Text("Enter your name: ")
+ Spacer()
+ TextField("Your name", text: $userName)
+ .frame(width: 150, height:30)
+ .multilineTextAlignment(.trailing)
+ .background(.white)
+ //.overlay(RoundedRectangle(cornerRadius: 2).stroke(.gray))
+ }
+ 
+ Toggle("Example setting 1", isOn: $settingOne)
+ Toggle("Example setting 2", isOn: $settingTwo)
+ Toggle("Example setting 3", isOn: $settingThree)
+ }
+ .padding(.leading, 20)
+ .padding(.trailing, 20)*/
